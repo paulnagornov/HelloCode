3012922466683de32a17d0331ae771e19dcd4514
pavelnagornov <paul.nagornov@gmail.com>
2022-12-18 16:58:23 +0500

    add Example 11 ArrayLibrary

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,13 @@
+﻿void FillArray(int[] collection);
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+
+        index++;
+    }
+}
+
+int[] array = new int[10];
